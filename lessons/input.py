62d7bd158a788e35b6d6100dbc3003e3298eebcf
August Clingroth <guscling@email.unc.edu>
2021-08-30 20:44:12 -0400

    Input.py with if else working

---
+++ lessons/input.py
@@ -0,0 +1,8 @@
+"""Examples of user input and variables"""
+
+user_name: str = input("Who are you? ")
+print("Wow, " + user_name + ", you rock!")
+print(user_name + ", have a great day!")
+Thanks: str = input ()
+if Thanks == ("Thanks") or Thanks == ("Thank you"): print("You're welcome, " + user_name + "!")
+else: print("Rude!")
